f6f8b613e2c3cb1ef17efb52ec0995a823bebb9a
Jona Petrikowski / PBS2H17A <jona.petrikowski@edu.bib.de>
2018-11-14 14:47:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pbs2h17ape/projectx/LayoutMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pbs2h17ape.projectx;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
